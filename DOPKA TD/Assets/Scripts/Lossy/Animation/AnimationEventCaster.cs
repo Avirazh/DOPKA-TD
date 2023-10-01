b2f7b7f515bcee5d81449070f814bab1743a3657
UnluckyDen <dsgladkih01@gmail.com>
2023-10-01 19:44:53 +0300

    animation

---
+++ DOPKA TD/Assets/Scripts/Lossy/Animation/AnimationEventCaster.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Lossy.Animation
+{
+    public class AnimationEventCaster : MonoBehaviour
+    {
+        public void OnDeadAnimationEnded()
+        {
+            
+        }
+    }
+}
